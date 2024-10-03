f916d7e0878f778f9747966cb9826e6050d45fb4
Douglas Alexandre <161042865+Titanzada@users.noreply.github.com>
2024-10-03 14:07:49 +0000

    atualizar

--- project/models/Endereco.py
+++ project/models/Endereco.py
@@ -1,11 +1,22 @@
 import sys
-sys.path.append("workspaces/Atividade-Em-Equipe/project")
+sys.path.append("/workspaces/Atividade-em-equipe")
+
 from project.models.enums.UnidadeFederativa import UnidadeFederativa
 
 class Endereco():
-    def __init__(self, logradouro:str, numero:str, complemento:str) -> None:
+    def __init__(self, logradouro:str, numero:str, complemento:str, uf:UnidadeFederativa) -> None:
         self.logradouro = logradouro
         self.numero = numero
         self.complemento = complemento
+        self.uf = uf
 
-    
+    def __str__(self) -> str:
+        return ( 
+                f"\nLogradouro: {self.logradouro}"
+                f"\nNúmero: {self.numero}"
+                f"\nComplemento: {self.complemento}"
+                f"\nUnidade Federativa: {self.uf.nome}"
+                )
+
+endereco = Endereco("Rua BEILA", "130", "1º Andar", UnidadeFederativa.BAHIA)
+print(endereco)
