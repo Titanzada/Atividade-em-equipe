e6fe32f351b926c683d49c764d9d73eb3b62cc7e
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-10-09 11:12:42 +0000

    atualizar

---
+++ project/abstracts/Pessoa.py
@@ -0,0 +1,14 @@
+from abc import ABC , abstractmethod
+from projeto.models import Endereco
+
+class Pessoa(ABC):
+    def __init__(self,nome:str, telefone :str, email:str,endereco: Endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.email= email
+        self.endereco = endereco
+   
+    @abstractmethod
+    def apresentar(self):
+        pass
+  
